e853d566fb92fca46664f3671794e8de46a1a9ec
marinarierav <marina.rierav@gmail.com>
2022-02-02 19:38:39 +0100

    feat: adds modal and functionality

--- src/components/Todo.js
+++ src/components/Todo.js
@@ -1,10 +1,28 @@
+import { useState } from "react";
+import Backdrop from "./Backdrop";
+import Modal from "./Modal";
+
 function Todo(props) {
+  const [isModalOpen, setIsModalOpen] = useState(false);
+
+  function deleteHandler() {
+    setIsModalOpen(true);
+  }
+
+  function closeModalHandler() {
+    setIsModalOpen(false);
+  }
+
   return (
     <div className="card">
       <h2>{props.text}</h2>
       <div className="actions">
-        <button className="btn">Delete</button>
+        <button className="btn" onClick={deleteHandler}>
+          Delete
+        </button>
       </div>
+      {isModalOpen && <Modal onCancel={closeModalHandler} />}
+      {isModalOpen && <Backdrop onClick={closeModalHandler} />}
     </div>
   );
 }
