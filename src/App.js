c64c5639a36556d5b34c7bc6544c305c17c19ed0
marinarierav <marina.rierav@gmail.com>
2022-02-02 21:05:11 +0100

    feat: adds deleteItem functionality

--- src/App.js
+++ src/App.js
@@ -14,12 +14,20 @@ function App() {
     setTodos(newTodos);
   };
 
+  const deleteTodo = (index) => {
+    const newTodos = [...todos];
+    newTodos.splice(index, 1);
+    setTodos(newTodos);
+  };
+
   return (
     <div>
       <h1 className="title">My Todos</h1>
       <div className="content">
         {todos.map((todo, index) => {
-          return <Todo text={todo.text}></Todo>;
+          return (
+            <Todo text={todo.text} index={index} onDelete={deleteTodo}></Todo>
+          );
         })}
         <TodoForm addTodo={addTodo} className="card" />
       </div>
--- src/components/Todo.js
+++ src/components/Todo.js
@@ -2,27 +2,34 @@ import { useState } from "react";
 import Backdrop from "./Backdrop";
 import Modal from "./Modal";
 
-function Todo(props) {
+function Todo({ text, index, onDelete }) {
   const [isModalOpen, setIsModalOpen] = useState(false);
 
-  function deleteHandler() {
+  function deleteButtonClicked() {
     setIsModalOpen(true);
+    return true;
   }
 
   function closeModalHandler() {
     setIsModalOpen(false);
+    return true;
   }
 
   return (
     <div className="list">
       <div className="card">
-        <h2>{props.text}</h2>
+        <h2>{text}</h2>
         <div className="actions">
-          <button className="btn" onClick={deleteHandler}>
+          <button className="btn" onClick={deleteButtonClicked}>
             Delete
           </button>
         </div>
-        {isModalOpen && <Modal onCancel={closeModalHandler} />}
+        {isModalOpen && (
+          <Modal
+            onCancel={closeModalHandler}
+            onConfirm={() => closeModalHandler() && onDelete(index)}
+          />
+        )}
         {isModalOpen && <Backdrop onClick={closeModalHandler} />}
       </div>
     </div>
